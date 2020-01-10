a19a2ebfda8e874a9a8eb5e81473f7f4ecc36176
Krystal Farwig <youngshaowei@gmail.com>
2020-01-09 19:33:48 -0600

    Last addition: displaySearchResults if statement added additional condition searchTerm=isBlank()

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -27,7 +27,7 @@ public class SearchController {
     @PostMapping(value = "/results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
         ArrayList<Job> jobs = new ArrayList<>();
-        if (searchType.equals("all")){
+        if (searchType.equals("all") || searchTerm.isBlank()){
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else if (!searchType.equals("all")){
